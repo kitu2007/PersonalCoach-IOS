c069b69b0c9ac162f639b843765a65369b6bc39e
Kg <nobody@nowhere.com>
2025-06-22 15:56:46 -0700

    Initial Commit

---
+++ PersonalCoach/PersonalCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCoachApp.swift
+//  PersonalCoach
+//
+//  Created by Kshitiz on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
